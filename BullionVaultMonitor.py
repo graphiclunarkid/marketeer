d841af0bbc931589e6568ceb765ac27d10da9b79
Richard King <mail@richardskingdom.net>
2011-12-31 21:06:24 +0000

    Refactored to specialise in Bullionvault only. Added more tests.

--- BullionVaultMonitor.py
+++ BullionVaultMonitor.py
@@ -1,7 +1,10 @@
 class BullionVaultMonitor():
-    def __init__(self):
-        self.currency = "GBP"
-        self.market = "AUXZU"
+    def __init__(self, currency, market):
+        self.currency = currency
+        self.market = market
         self.ask = 32490
         self.bid = 32410
         self.spread = self.ask - self.bid
+
+    def dispose(self):
+        pass
--- Test_BullionVaultMonitor.py
+++ Test_BullionVaultMonitor.py
@@ -1,13 +1,37 @@
 import unittest
 import BullionVaultMonitor
 
-class Test_BullionVaultMonitor(unittest.TestCase):
+class Test_BullionVaultMonitorMarkets(unittest.TestCase):
 
     def setUp(self):
-        self.monitor = BullionVaultMonitor.BullionVaultMonitor()
-        self.currencies = set( ['EUR','GBP','USD',''] ) # Blank = all
-        self.markets = set( ['AUXLN','AUXNY','AUXZU',''] ) # Blank = all
+        self.validCurrencies = list( ['EUR','GBP','USD',''] ) # Blank = all
+        self.validMarkets = list( ['AUXLN','AUXNY','AUXZU',''] ) # Blank = all
+        self.monitors = set()
+
+        for i in self.validCurrencies:
+            for j in self.validMarkets:
+                self.monitors.add(BullionVaultMonitor.BullionVaultMonitor(i,j))
         
+    def test_Markets(self):
+        while len(self.monitors) > 0:
+            try:
+                self.monitor = self.monitors.pop()
+                self.assertIn(self.monitor.currency, self.validCurrencies, "Invalid currency")
+                self.assertIn(self.monitor.market, self.validMarkets, "Invalid market")
+            except KeyError, e:
+                self.assertEqual(len(self.monitors), 0, e.message)
+
+    def tearDown(self):
+        self.monitors = None
+        self.currencies = None
+        self.markets = None
+
+
+class Test_BullionVaultMonitorPrices(unittest.TestCase):
+
+    def setUp(self):
+        self.monitor = BullionVaultMonitor.BullionVaultMonitor('EUR', 'AUXLN')
+    
     def test_Prices(self):
         self.assertGreater(self.monitor.ask, 0, "Ask price is negative")
         self.assertGreater(self.monitor.bid, 0, "Bid price is negative")
@@ -15,9 +39,9 @@ class Test_BullionVaultMonitor(unittest.TestCase):
         self.assertGreaterEqual(self.monitor.ask, self.monitor.bid, "Ask price is >= bid price")
         self.assertGreaterEqual(self.monitor.spread, 0, "Spread is negative")
 
-    def test_Markets(self):
-        self.assertIn(self.monitor.currency, self.currencies, "Invalid currency")
-        self.assertIn(self.monitor.market, self.markets, "Invalid market")
+    def tearDown(self):
+        self.monitor.dispose()
+        self.monitor = None
 
 
 if __name__ == '__main__':
