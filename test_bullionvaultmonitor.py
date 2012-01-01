6c798a4746ab73a8d7f0a7557f1ad3d93d9f322e
Richard King <mail@richardskingdom.net>
2012-01-01 22:38:52 +0000

    Fixed a bug by refactoring more tests into test_monitor from test_bullionvaultmonitor

--- test_bullionvaultmonitor.py
+++ test_bullionvaultmonitor.py
@@ -18,21 +18,6 @@ class Test_BullionVaultMonitor(test_monitor.Test_Monitor):
             for j in self.validMarkets:
                 self.validMonitors.add(bullionvaultmonitor.BullionVaultMonitor(self.updatePeriod,i,j))
 
-    def tearDown(self):
-
-        self.validMonitors.clear()
-        self.validMonitors = None
-        self.monitors = None
-
-    def test_getMonitorAttributes(self):
-
-        self.monitors = self.validMonitors.copy()
-
-        while len(self.monitors) > 0:
-
-            self.monitor = self.monitors.pop()
-            test_monitor.Test_Monitor.test_getMonitorAttributes(self)
-        
     def test_invalidMonitors(self):
 
         with self.assertRaises(monitor.InitError) as cm:
--- test_monitor.py
+++ test_monitor.py
@@ -6,24 +6,37 @@ class Test_Monitor(unittest.TestCase):
     def setUp(self):
         self.url = 'http://live.bullionvault.com/view_market_xml.do'
         self.updatePeriod = 60
-        self.currency = 'GBP'
-        self.market = 'AUXLN'
-        self.monitor = monitor.Monitor(self.url,self.updatePeriod,self.currency,self.market)
+        self.validCurrencies = 'GBP'
+        self.validMarkets = 'AUXLN'
+
+        self.validMonitors = set()
+
+        for i in self.validCurrencies:
+            for j in self.validMarkets:
+                self.validMonitors.add(monitor.Monitor(self.url,self.updatePeriod,i,j))
 
     def tearDown(self):
+
+        self.validMonitors.clear()
+        self.validMonitors = None
+        self.monitors = None
         self.monitor = None
 
-    def suite():
-        return unittest.TestLoader().loadTestsFromTestCase(Test_Monitor)
-    
     def test_getMonitorAttributes(self):
-        self.assertIsNotNone(self.monitor.getCurrency(), 'Currency is not set')
-        self.assertIsNotNone(self.monitor.getMarket(), 'Market is not set')
-        self.assertGreater(self.monitor.getBid(), 0, 'Bid price is zero or negative')
-        self.assertGreater(self.monitor.getOffer(), 0, 'Offer price is zero or negative')
-        self.assertGreaterEqual(self.monitor.getSpread(), 0, 'Spread is negative')
-        self.assertIsNotNone(self.monitor.getUrl(), 'URL not set')
-        self.assertGreater(self.monitor.getUpdatePeriod(), 0, 'Update period is zero or negative')
+
+        self.monitors = self.validMonitors.copy()
+
+        while len(self.monitors) > 0:
+
+            self.monitor = self.monitors.pop()
+
+            self.assertIsNotNone(self.monitor.getCurrency(), 'Currency is not set')
+            self.assertIsNotNone(self.monitor.getMarket(), 'Market is not set')
+            self.assertGreater(self.monitor.getBid(), 0, 'Bid price is zero or negative')
+            self.assertGreater(self.monitor.getOffer(), 0, 'Offer price is zero or negative')
+            self.assertGreaterEqual(self.monitor.getSpread(), 0, 'Spread is negative')
+            self.assertIsNotNone(self.monitor.getUrl(), 'URL not set')
+            self.assertGreater(self.monitor.getUpdatePeriod(), 0, 'Update period is zero or negative')
 
 if __name__ == '__main__':
     unittest.main()
