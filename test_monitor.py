aebbf1eb500329dea07beeaf1aa7fa7f971d024b
Richard King <mail@richardskingdom.net>
2012-01-01 23:03:56 +0000

    Small change to make a test more efficient

--- test_monitor.py
+++ test_monitor.py
@@ -17,18 +17,17 @@ class Test_Monitor(unittest.TestCase):
 
     def tearDown(self):
 
-        self.validMonitors.clear()
+        if len(self.validMonitors) > 0:
+            self.validMonitors.clear()
+
         self.validMonitors = None
-        self.monitors = None
         self.monitor = None
         
     def test_getMonitorAttributes(self):
 
-        self.monitors = self.validMonitors.copy()
+        while len(self.validMonitors) > 0:
 
-        while len(self.monitors) > 0:
-
-            self.monitor = self.monitors.pop()
+            self.monitor = self.validMonitors.pop()
 
             self.assertIsNotNone(self.monitor.getCurrency(), 'Currency is not set')
             self.assertIsNotNone(self.monitor.getMarket(), 'Market is not set')
@@ -37,6 +36,12 @@ class Test_Monitor(unittest.TestCase):
             self.assertGreaterEqual(self.monitor.getSpread(), 0, 'Spread is negative')
             self.assertIsNotNone(self.monitor.getUrl(), 'URL not set')
             self.assertGreater(self.monitor.getUpdatePeriod(), 0, 'Update period is zero or negative')
+
+#    def test_setMonitorAttributes(self):
+#
+#        self.monitor = self.validmonitors.pop()
+#        self.monitor.setUpdatePeriod(90)
+#        self.assertEqual(self.monitor.getUpdatePeriod, 90, 'Update period was not set correctly')
 
 suite = unittest.TestLoader().loadTestsFromTestCase(Test_Monitor)
 unittest.TextTestRunner(verbosity=2).run(suite)
