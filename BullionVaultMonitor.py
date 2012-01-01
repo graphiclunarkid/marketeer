92a680ca28a8006c77470fe02334c9460908520d
Richard King <mail@richardskingdom.net>
2012-01-01 14:12:31 +0000

    Removed 'all currencies/markets' option. Added get methods for monitor attributes.

--- BullionVaultMonitor.py
+++ BullionVaultMonitor.py
@@ -1,23 +1,39 @@
+#!/usr/bin/python
+
 class BullionVaultMonitor():
 
     def __init__(self, currency, market):
 
-        self.offer = 32490
-        self.bid = 32410
-        self.spread = self.offer - self.bid
-
         self.currency = currency
         self.market = market
 
-        ''' Blank = monitor all markets / currencies '''
-        self.validCurrencies = frozenset( ['EUR','GBP','USD',''] )
-        self.validMarkets = frozenset( ['AUXLN','AUXNY','AUXZU',''] )
+        self.validCurrencies = frozenset( ['EUR','GBP','USD'] )
+        self.validMarkets = frozenset( ['AUXLN','AUXNY','AUXZU'] )
 
         if self.currency not in self.validCurrencies:
             raise InitError('Invalid currency')
 
         if self.market not in self.validMarkets:
             raise InitError('Invalid market')
+
+        self.offer = 32490
+        self.bid = 32410
+        self.spread = self.offer - self.bid
+
+    def getCurrency(self):
+        return self.currency
+
+    def getMarket(self):
+        return self.market
+
+    def getBid(self):
+        return self.bid
+
+    def getOffer(self):
+        return self.offer
+    
+    def getSpread(self):
+        return self.spread
 
 
 class Error(Exception):
--- Test_BullionVaultMonitor.py
+++ Test_BullionVaultMonitor.py
@@ -5,8 +5,8 @@ class Test_BullionVaultMonitor(unittest.TestCase):
 
     def setUp(self):
 
-        self.validCurrencies = list( ['EUR','GBP','USD',''] ) # Blank = all
-        self.validMarkets = list( ['AUXLN','AUXNY','AUXZU',''] ) # Blank = all
+        self.validCurrencies = list( ['EUR','GBP','USD'] )
+        self.validMarkets = list( ['AUXLN','AUXNY','AUXZU'] )
         self.invalidCurrency = '123'
         self.invalidMarket = '456'
         self.validMonitors = set()
@@ -27,6 +27,27 @@ class Test_BullionVaultMonitor(unittest.TestCase):
             self.assertGreater(self.monitor.bid, 0, 'Bid price is negative')
             self.assertGreaterEqual(self.monitor.offer, self.monitor.bid, 'Offer price is >= bid price')
             self.assertGreaterEqual(self.monitor.spread, 0, 'Spread is negative')
+
+    def test_getMonitorAttributes(self):
+
+        self.monitors = self.validMonitors.copy()
+
+        while len(self.monitors) > 0:
+
+            self.monitor = self.monitors.pop()
+
+            self.currency = self.monitor.getCurrency()
+            self.market = self.monitor.getMarket()
+            self.bid = self.monitor.getBid()
+            self.offer = self.monitor.getOffer()
+            self.spread = self.monitor.getSpread()
+
+            self.assertIn(self.market, self.validMarkets, 'Invalid market')
+            self.assertIn(self.currency, self.validCurrencies, 'Invalid currency')
+            self.assertGreater(self.offer, 0, 'Offer price is negative')
+            self.assertGreater(self.bid, 0, 'Bid price is negative')
+            self.assertGreaterEqual(self.offer, self.bid, 'Offer price is >= bid price')
+            self.assertGreaterEqual(self.spread, 0, 'Spread is negative')
 
     def test_invalidMonitors(self):
 
