975f6f0df6150ef0cdb986840223ebdef6ba5599
Richard King <mail@richardskingdom.net>
2013-02-15 21:55:14 +0000

    Created a template monitor class + tests for MtGox

---
+++ mtgoxmonitor.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+# Copyright 2013 Richard King
+#
+# This file is part of Marketeer.
+#
+# Marketeer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Marketeer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Marketeer.  If not, see <http://www.gnu.org/licenses/>.
+
+class MtgoxMonitor():
+    '''
+    Class to monitor market prices at MtGox
+    '''
+
+    def __init__(self, updatePeriod):
+        self.updatePeriod = updatePeriod
+
+    def _update(self):
+
+        self._bid = 20
+        self._offer = 22
+
+    def get_bid(self):
+
+        self._update()
+        return self._bid
+
+    bid = property(get_bid)
+
+    def get_offer(self):
+
+        self._update()
+        return self._offer
+
+    offer = property(get_offer)
+
+    def get_spread(self):
+
+        self._update()
+        return self._offer - self._bid
+
+    spread = property(get_spread)
+
+
---
+++ test_mtgoxmonitor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+# Copyright 2013 Richard King
+#
+# This file is part of Marketeer.
+#
+# Marketeer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Marketeer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Marketeer.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+import mtgoxmonitor
+
+class Test_MtgoxMonitor(unittest.TestCase):
+
+    def setUp(self):
+        self.updatePeriod = 30
+
+    def test_monitorAttributes(self):
+
+        monitor = mtgoxmonitor.MtgoxMonitor(self.updatePeriod)
+        self.assertIsNotNone(monitor.updatePeriod, 'Update period is not set')
+        self.assertEqual(monitor.updatePeriod, self.updatePeriod, 'Update period not set correctly')
+
+        self.assertIsNotNone(monitor.spread, 'Spread is not set')
+        self.assertGreaterEqual(monitor.spread, 0, 'Spread is negative')
+        self.assertIsNotNone(monitor.bid, 'Bid is not set')
+        self.assertIsNotNone(monitor.offer, 'Offer is not set')
+
