6d17790c45834f18e97d4b70c8a8bef9ad3ea53c
Richard King <mail@richardskingdom.net>
2014-09-20 17:27:14 +0200

    Package project for distribution

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+classifiers = []
+classifiers.append('Development Status :: 2 - Pre-Alpha')
+classifiers.append('Environment :: Console')
+classifiers.append('Intended Audience :: Developers')
+classifiers.append('Intended Audience :: End Users/Desktop')
+classifiers.append('Intended Audience :: Financial and Insurance Industry')
+classifiers.append('Intended Audience :: Science/Research')
+classifiers.append('License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)')
+classifiers.append('Operating System :: OS Independent')
+classifiers.append('Programming Language :: Python')
+classifiers.append('Programming Language :: Python :: 3')
+classifiers.append('Topic :: Office/Business :: Financial')
+classifiers.append('Topic :: Office/Business :: Financial :: Investment')
+classifiers.append('Topic :: Scientific/Engineering :: Information Analysis')
+
+setup(
+    name = 'marketeer',
+    version = '0.1.2',
+    author = 'Richard King',
+    author_email = 'mail@richardskingdom.net',
+    url = 'https://github.com/graphiclunarkid/marketeer',
+    packages = ['marketeer'],
+    description = 'Trading bot',
+    long_description = '',
+    classifiers = classifiers,
+)
